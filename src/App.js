949b95285178e2a0a4a2180cffc5819f86743d66
Simon <simonh72498@gmail.com>
2020-05-13 19:26:29 -0400

    finished mvp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [imgLink, setImgLink] = useState("")
+  const [date, setDate] = useState("");
+  const [explanation, setExplanation] = useState("");
+  const [title, setTitle] = useState("");
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=jEO61xQVaCf47CVVM7eVDyA74QXSPIZ3YpUOVaqU")
+      .then(res => { 
+        setImgLink(res.data.url)
+        setDate(res.data.date)
+        setExplanation(res.data.explanation)
+        setTitle(res.data.title)
+      })
+  }, [])
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>{title}</h1>
+      <h3>{date}</h3>
+      <img src={imgLink} alt=""/>
+      <p>{explanation}</p>
     </div>
   );
 }
